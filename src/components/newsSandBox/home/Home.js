508a208e898000d83b440e20d3b07cab0f966beb
王赟诚 <w729567588@163.com>
2022-07-14 11:13:01 +0800

    SideMenu

--- src/components/newsSandBox/home/Home.js
+++ src/components/newsSandBox/home/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {Button} from "antd";
+import axios from "axios";
 
 export default function Home() {
+    const ajax = () => {
+        axios.get("http://localhost:5050/users").then(res => {
+            console.log(res.data)
+        })
+    }
+
     return (
         <div>
             home
+            <Button type="primary" onClick={ajax}>111</Button>
         </div>
     )
 }
--- src/components/newsSandBox/side-menu/SideMenu.js
+++ src/components/newsSandBox/side-menu/SideMenu.js
@@ -1,37 +1,70 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {withRouter} from "react-router-dom";
+import axios from "axios";
 
 import {Layout, Menu} from "antd";
 import style from "../side-menu/SideMenu.module.scss";
-import {FormOutlined, HomeOutlined, UserOutlined} from "@ant-design/icons";
+import {
+    FormOutlined,
+    HomeOutlined,
+    UserOutlined,
+    InfoCircleOutlined,
+    AuditOutlined,
+    BarChartOutlined
+} from "@ant-design/icons";
 
 const {Sider} = Layout;
 
-export default function SideMenu() {
+function SideMenu(props) {
     let collapsed = false;
+    const [menu, setMenu] = useState([]);
 
-    function getItem(label, key, icon, children, type) {
-        return {
-            key, icon, children, label, type,
-        };
+    const iconList = {
+        "/home": <HomeOutlined/>,
+        "/user-manage": <UserOutlined/>,
+        "/right-manage": <FormOutlined/>,
+        "/news-manage": <InfoCircleOutlined/>,
+        "/audit-manage": <AuditOutlined/>,
+        "/publish-manage": <BarChartOutlined/>
     }
 
-    const items = [
-        getItem("首页", "index", <HomeOutlined/>),
-        getItem("用户管理", "user-manage", <UserOutlined/>, [
-            getItem("用户列表", "user-list")
-        ]),
-        getItem("权限管理", "right-manage", <FormOutlined/>, [
-            getItem("角色列表", "role-list"),
-            getItem("权限列表", "right-list")
-        ]),
-    ]
+    useEffect(() => {
+        axios.get("http://localhost:5050/rights?_embed=children").then(res => {
+            // console.log(res.data);
+            let data = res.data;
+            let new_data = JSON.stringify(data);
+            let _new_data = new_data.replace(/"title"/g, '"label"').replace(/"rightId"/g, '"rightid"');
+            // console.log(_new_data);
+            data = JSON.parse(_new_data);
+            data.forEach(item => {
+                item.icon = iconList[item.key];
+                // 移除空children节点
+                if (item.children.length === 0) {
+                    delete item["children"];
+                }
+                // console.log(item.children);
+                // 移除无pagepermission权限节点
+                if (item.children !== undefined) {
+                    for (let i = 0; i < item.children.length; i++) {
+                        if (item.children[i].pagepermission === undefined) {
+                            delete item.children[i]
+                        }
+                    }
+                }
+
+            });
+            setMenu(data);
+        })
+    }, []);
 
     return (
         <Sider className={style.container} width={"15rem"} collapsible collapsed={collapsed} theme="dark"
                trigger={null}>
             <div className={style.title}>全球新闻发布管理系统</div>
-            <Menu theme="dark" defaultSelectedKeys={['index']} defaultOpenKeys={['user-manage', 'right-manage']}
-                  items={items}/>
+            <Menu theme="dark" defaultSelectedKeys={[props.location.pathname]} defaultOpenKeys={["/"+props.location.pathname.split("/")[1]]}
+                  mode="inline" inlineCollapsed={collapsed} items={menu} onClick={(e) => {props.history.push(e.key)}}/>
         </Sider>
     )
 }
+
+export default withRouter(SideMenu);
